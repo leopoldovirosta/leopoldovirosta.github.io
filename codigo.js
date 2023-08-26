842b7ca03cc4da473782968ee30a335f2cff9951
leopoldovirosta <leopoldo072@gmail.com>
2023-08-26 14:55:42 +0200

    añadido formulario

--- codigo.js
+++ codigo.js
@@ -1,7 +1,29 @@
-function mostraralerta(){
-    alert('hizo clic!');
+function iniciar(){
+    edad1=document.getElementById("edad");
+    nombrem1=document.getElementById("nombrem");
+    dnim1=document.getElementById("dnim");
+    edad1.addEventListener("input", validacion, false);
+    nombrem1.addEventListener("input", validacion, false);
+    dnim1.addEventListener("input", validacion, false);
+}
+function validacion(){
+    if(edad1.value<18){
+        if(nombrem1.value==''){
+            nombrem1.setCustomValidity('inserte un nombre');
+            nombrem1.style.background='#FFDDDD';
+        }else{
+            nombrem1.setCustomValidity('');
+            nombrem1.style.background='#FFFFFF';
+        }
+        if(dnim1.value==''){
+            dnim1.setCustomValidity('inserte un dni');
+            dnim1.style.background='#FFDDDD';
+        }else{
+            dnim1.setCustomValidity('');
+            dnim1.style.background='#FFFFFF';
+        }
     }
-function hacerclic(){
-    document.getElementsByTagName('p')[0].onclick=mostraralerta;
-    }
-window.onload=hacerclic;
+}
+
+
+window.addEventListener('load', iniciar, false);
